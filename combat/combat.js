0c7a8e0aee8b2bcded40da60c9ea86c8fd52e289
HuLijin <hu.lijin@zoho.com>
2016-11-12 22:31:04 +0100

    Reset violent des fonts (parce que) et qq manoeuvres

--- combat/combat.js
+++ combat/combat.js
@@ -1,18 +1,6 @@
 function toggle(name){
     var elt = document.getElementsByClassName(name);
     var elt2 = document.getElementsByClassName(name+"Link");
-    console.log(elt2);
-    /*if (elt.length == 0) { 
-        var hidden = name+" hidden";
-        elt = document.getElementsByClassName(hidden);
-        for (var i = 0; i < elt.length; i++) {
-            elt[i].className = name;
-        }
-    } else {
-        for (var i = 0; i < elt.length; i++) {
-            elt[i].className = name+" hidden";
-        }
-    }*/
     for (var i = 0; i < elt.length; i++) {
         if (elt[i].className == name+" hidden") { 
             elt[i].className = name; 
--- competences/competences.js
+++ competences/competences.js
@@ -1,26 +1,25 @@
 function toggle(name){
     var elt = document.getElementsByClassName(name);
     var elt2 = document.getElementsByClassName(name+"Link");
-    console.log(elt2);
-    /*if (elt.length == 0) { 
-        var hidden = name+" hidden";
-        elt = document.getElementsByClassName(hidden);
-        for (var i = 0; i < elt.length; i++) {
-            elt[i].className = name;
-        }
-    } else {
-        for (var i = 0; i < elt.length; i++) {
-            elt[i].className = name+" hidden";
-        }
-    }*/
     for (var i = 0; i < elt.length; i++) {
         if (elt[i].className == name+" hidden") { 
             elt[i].className = name; 
             elt2[0].innerHTML = "(cacher)";
         }
         else { 
-            elt[i].className = name+" hidden"; 
-            elt2[0].innerHTML = "(afficher)";
+            if (elt[i].className == name+" hidden eg") { 
+            elt[i].className = name+" eg"; 
+            elt2[0].innerHTML = "(cacher)";
+            }
+            else { 
+                if (elt[i].className == name+" eg") { 
+                    elt[i].className = name+" hidden eg"; 
+                    elt2[0].innerHTML = "(afficher)";
+                } else {
+                    elt[i].className = name+" hidden"; 
+                    elt2[0].innerHTML = "(afficher)";
+                }
+            }
         }
     }
 }
