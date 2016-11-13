a02ed9f1f357d39fc01a61af4dad864c16d66309
HuLijin <hu.lijin@zoho.com>
2016-11-13 19:43:33 +0100

    Mise au propre combat

--- combat/inc_aside.js
+++ combat/inc_aside.js
@@ -1,4 +1,6 @@
 document.write("<h3>Navigation rapide</h3>");
 document.write("<nav class='menu'>");
-document.write("<a href='#'>Page principale</a>");
+document.write("<a href='./combat.html'>L'art du combat</a>");
+document.write("<a href='./manoeuvres.html'>Les manoeuvres</a>");
+document.write("<a href='./tactiques_avancees.html'>Tactiques avancées</a>");
 document.write("</nav>");
