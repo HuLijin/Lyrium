56e5b81ed3286ffb6de89bb0b1608c13808ca5db
HuLijin <hu.lijin@zoho.com>
2016-11-15 00:32:16 +0100

    quelques mises au point en css

--- voyage/voyage.js
+++ voyage/voyage.js
@@ -10,3 +10,17 @@ function calculate(pieds) {
     var tired = base - parseInt(quality)*10;
     alert("Temps de trajet : "+hour+"h"+min+"\nFatigue : "+tired);
 }
+
+function toggle(name){
+    var elt = document.getElementsByClassName(name);
+    var elt2 = document.getElementsByClassName(name+"Link");
+    for (var i = 0; i < elt.length; i++) {
+        if (elt[i].className == name+" hidden boxed") { 
+            elt[i].className = name+" boxed"; 
+            elt2[0].innerHTML = "(cacher la description des "+name+")";
+        } else { 
+            elt[i].className = name+" hidden boxed"; 
+            elt2[0].innerHTML = "(afficher la description des "+name+")";
+        }
+    }
+}
