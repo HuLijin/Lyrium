07a6a5dcd26e761e90727a6049d73df92c228f3e
HuLijin <hu.lijin@zoho.com>
2016-11-15 15:56:36 +0100

    Affichage tableaux commerce

--- voyage/inc_aside.js
+++ voyage/inc_aside.js
@@ -1,4 +1,5 @@
 document.write("<h3>Navigation rapide</h3>");
 document.write("<nav class='menu'>");
-document.write("<a href='#'>Page principale</a>");
+document.write("<a href='./voyage.html'>Règles du voyage</a>");
+document.write("<a href='./objets_voyage.html'>Liste d'objets à acheter</a>");
 document.write("</nav>");
