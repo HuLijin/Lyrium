03f8466caee98e3581a801ff943e646c40be4de6
HuLijin <hu.lijin@zoho.com>
2016-11-17 15:45:33 +0100

    Mise à jour domaines

--- magie/magie.js
+++ magie/magie.js
@@ -1,3 +1,17 @@
+function toggle(name){
+    var elt = document.getElementsByClassName(name);
+    var elt2 = document.getElementsByClassName(name+"Link");
+    for (var i = 0; i < elt.length; i++) {
+        if (elt[i].className == name+" hidden") { 
+            elt[i].className = name; 
+            elt2[0].innerHTML = "(cacher)";
+        } else { 
+            elt[i].className = name+" hidden"; 
+            elt2[0].innerHTML = "(afficher)";
+        }
+    }
+}
+
 function calculateDice() {
     var dice = parseInt(document.calculateur.dice.value);
     
