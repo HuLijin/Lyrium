4304904eb9b8a93a56ad8288695e9a04b1d272ef
HuLijin <hu.lijin@zoho.com>
2016-11-19 22:58:33 +0100

    Ajouts mineurs et ajout PNJ

---
+++ personnage/PJ/inc_aside.js
@@ -0,0 +1,6 @@
+document.write("<h3>PJ par archétype</h3>");
+document.write("<h4>Les magiciens</h4>");
+document.write("<nav class='menu'>");
+document.write("<a href='./Barol.html'>Barol</a>");
+document.write("<a href='./LalianVess.html'>Lalian Vess</a>");
+document.write("</nav>");
