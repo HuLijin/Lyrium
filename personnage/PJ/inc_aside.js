92c31e145473d2c86d00735b566c02731e1d08d5
HuLijin <hu.lijin@zoho.com>
2016-11-21 22:39:03 +0100

    Qq ajustements mineurs

--- personnage/PJ/inc_aside.js
+++ personnage/PJ/inc_aside.js
@@ -1,9 +1,12 @@
-document.write("<h3>PJ par archétype</h3>");
+document.write("<h3>PJ par archétypes</h3>");
+document.write("<a href='./HUB.html'>&crarr; Retour au HUB des personnages</a>")
 document.write("<h4>Les combattants</h4>");
+document.write("<p class='eg'>Pas encore de combattants</p>")
 document.write("<h4>Les magiciens</h4>");
 document.write("<nav class='menu'>");
 document.write("<a href='./Barol.html'>Barol</a>");
 document.write("<a href='./LalianVess.html'>Lalian Vess</a>");
+document.write("<a href='./YeoundErayo.html'>Yeoun d'Erayo</a>");
 document.write("</nav>");
 document.write("<h4>Les inclassables</h4>");
 document.write("<nav class='menu'>");
