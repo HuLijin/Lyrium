4675bd9233043411fb2f9d79a42da6146bbaa720
HuLijin <hu.lijin@zoho.com>
2016-11-21 22:43:31 +0100

    ajout liens

--- personnage/inc_aside.js
+++ personnage/inc_aside.js
@@ -1,5 +1,6 @@
 document.write("<h3>Navigation rapide</h3>");
 document.write("<nav class='menu'>");
-document.write("<a href='#'>Page principale</a>");
-document.write("<a href='gen_personnage.html'>Générateur de eprsonnage</a>");
+document.write("<a href='./personnage.html'>Page principale</a>");
+document.write("<a href='./gen_personnage.html'>Générateur de personnage</a>");
+document.write("<a href='./PJ/HUB.html'>Liste des personnages créés</a>")
 document.write("</nav>");
