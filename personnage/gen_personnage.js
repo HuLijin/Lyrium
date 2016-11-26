d525858733a2c265713664f182456becb1eb745a
HuLijin <hu.lijin@zoho.com>
2016-11-26 13:13:40 +0100

    ajout manoeuvres

--- personnage/gen_personnage.js
+++ personnage/gen_personnage.js
@@ -1,4 +1,6 @@
-function display(cat) {
+/* CHOIX TYPE DE MAGICIEN */
+
+function displayMagie(cat) {
     document.getElementById("mage-profane").className = "hidden";
     document.getElementById("mage-elementaire").className = "hidden";
     document.getElementById(cat).className = "";
@@ -6,17 +8,7 @@ function display(cat) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+/* CARACTERISTIQUES PRIMAIRES */
 
 function getFOR () { return parseInt(document.getElementById("for").value); }
 function getAGI () { return parseInt(document.getElementById("agi").value); }
@@ -52,6 +44,15 @@ function updateINT() {
     computeCaracPts();
 
     /* Mise à jour des valeurs dans la feuille de personnage */
+}
+
+function INTtoDice(intel) {
+
+    if (intel < 5) { return "1D4"; }
+    if (intel < 6) { return "1D6"; }
+    if (intel < 7) { return "1D8"; }
+    if (intel < 5) { return "1D10"; }
+    return "1D12";
 }
 
 function updateMEM() {
@@ -109,42 +110,13 @@ function computeCaracPts() {
 
 
 
-function update() {
+/* COMPETENCES */
 
-    var FOR = parseInt(document.caracPrincipales.valFOR.value);
-    var AGI = parseInt(document.caracPrincipales.valAGI.value);
-    var CON = parseInt(document.caracPrincipales.valCON.value);
-    var INTEL = parseInt(document.caracPrincipales.valINT.value);
-    var MEM = parseInt(document.caracPrincipales.valMEM.value);
-    var VOL = parseInt(document.caracPrincipales.valVOL.value);
-    var CHA = parseInt(document.caracPrincipales.valCHA.value);
-
-    var ans = 37;
-    ans = ans - FOR - AGI - CON - INTEL - MEM - VOL - CHA;
-    document.getElementById("answerPts").innerHTML = ans;
-
-    /* Mise à jour de la section du bas */
-    /*var PV = CON+FOR+VOL;
-    document.getElementById("infoPV").innerHTML = PV;
-    var PF = CON+AGI+VOL;
-    document.getElementById("infoPF").innerHTML = PF;
-    var PM = MEM+INTEL+VOL;
-    document.getElementById("infoPM").innerHTML = PM;
-    var MAE = FOR+AGI;
-    document.getElementById("infoMAE").innerHTML = MAE;
-    var POU = INTEL+MEM;
-    document.getElementById("infoPOU").innerHTML = POU;
-    var ADA = INTEL+AGI;
-    document.getElementById("infoADA").innerHTML = ADA;*/
-
-    /* Autres informations complémentaires */
-    /*var pdm = MEM+1;
-    document.getElementById("infoPdm").innerHTML = pdm;
-    document.getElementById("infoINT").innerHTML = INTtoDice(INTEL);*/
-
-    return ans;
+function displayComp(cat) {
+    document.getElementById("specialiste").className = "hidden";
+    document.getElementById("touche-a-tout").className = "hidden";
+    document.getElementById(cat).className = "";
 }
-
 
 function updateComp(number) {
     var comp = "comp"+number;
@@ -168,49 +140,31 @@ function computeCompPts() {
     return pts;
 }
 
-function INTtoDice(intel) {
 
-    if (intel < 5) { return "1D4"; }
-    if (intel < 6) { return "1D6"; }
-    if (intel < 7) { return "1D8"; }
-    if (intel < 5) { return "1D10"; }
-    return "1D12";
 
+/* MANOEUVRES */
+
+function toggleManoeuvres() {
+    var inner = document.getElementById("link-form-manoeuvres").innerHTML;
+    if (inner == "Afficher") {
+        document.getElementById("link-form-manoeuvres").innerHTML = "Cacher";
+        document.getElementById("form-manoeuvres").className = "";
+    } else {
+        document.getElementById("link-form-manoeuvres").innerHTML = "Afficher";
+        document.getElementById("form-manoeuvres").className = "hidden";
+    }
 }
 
-function validateStats() {
-    
-    var ans = update();
-    var s = "s";
-
-    if (ans > 0) {
-        if (ans == 1) { s = ""; }
-        document.getElementById("answerVal").innerHTML = "Il reste "+ans+" point"+s+" à investir !";
-    } else {
-        if (ans < 0) {
-            if (ans == -1) { s = ""; }
-            document.getElementById("answerVal").innerHTML = "Le personnage a "+(-ans)+"point"+s+" en trop !";
+function updateManoeuvre(cat = "") {
+    if (cat != "") {
+        var val = parseInt(document.getElementById(cat+"1").value);
+        if (val == 0) {
+            document.getElementById("manoeuvre-"+cat).className = "hidden";
+            document.getElementById("manoeuvre-"+cat+"-bonus").className = "eg hidden";
         } else {
-            var deltaFOR = parseInt(document.caracPrincipales.valFOR.value) - 5;
-            var deltaAGI = parseInt(document.caracPrincipales.valAGI.value) - 5;
-            var deltaCON = parseInt(document.caracPrincipales.valCON.value) - 5;
-            var deltaINT = parseInt(document.caracPrincipales.valINT.value) - 5;
-            var deltaMEM = parseInt(document.caracPrincipales.valMEM.value) - 5;
-            var deltaVOL = parseInt(document.caracPrincipales.valVOL.value) - 5;
-            var deltaCHA = parseInt(document.caracPrincipales.valCHA.value) - 5;
-            var delta = 0;
-            if (deltaFOR < 0) { delta = delta + deltaFOR; }
-            if (deltaAGI < 0) { delta = delta + deltaAGI; }
-            if (deltaCON < 0) { delta = delta + deltaCON; }
-            if (deltaINT < 0) { delta = delta + deltaINT; }
-            if (deltaMEM < 0) { delta = delta + deltaMEM; }
-            if (deltaVOL < 0) { delta = delta + deltaVOL; }
-            if (deltaCHA < 0) { delta = delta + deltaCHA; }
-            if (delta < -4) {
-                document.getElementById("answerVal").innerHTML = "Le personnage n'est pas valide, ses statistiques sont trop extrêmes, augmentez "+(-delta-4)+" de vos statistiques les plus faibles et baissez "+(-delta-4)+" de vos statistiques les plus fortes (vous n'avez droit de déplacer que 4 points d'une statistique à l'autre).";
-            } else {
-                document.getElementById("answerVal").innerHTML = "Le personnage est parfaitement valide.";
-            }
+            document.getElementById("manoeuvre-"+cat).className = "";
+            document.getElementById("manoeuvre-"+cat+"-bonus").className = "eg";
         }
     }
+    /* Calculer tout plein de trucs */
 }
