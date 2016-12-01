14b8297216c1d9155e33816cef59ed1eddf645a1
HuLijin <hu.lijin@zoho.com>
2016-12-01 07:17:38 +0100

    maj polices perso

--- personnage/inc_aside.js
+++ personnage/inc_aside.js
@@ -2,6 +2,7 @@ document.write("<h3>Navigation rapide</h3>");
 document.write("<nav class='menu'>");
 document.write("<a href='./personnage.html'>Page principale</a>");
 document.write("<a href='./competences.html'>Les compétences</a>");
+document.write("<a href='./talents.html'>Les talents</a>");
 document.write("<a href='./gen_personnage.html'>Générateur de personnage</a>");
 document.write("<a href='./PJ/HUB.html'>Liste des personnages créés</a>")
 document.write("</nav>");
